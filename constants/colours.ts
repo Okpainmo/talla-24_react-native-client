0dabcaf7184dd325f641f1765bc7e424f38e2b22
Okpainmo <okpainmoandrew@gmail.com>
2024-09-06 22:14:55 +0100

    chore: project color(light theme) update

--- constants/colours.ts
+++ constants/colours.ts
@@ -6,7 +6,7 @@ export const default_light_backgrounds = {
 };
 
 export const default_light_texts = {
-  text_variant_1: '#000000', // black
+  text_variant_1: '#6b7280', // black
   text_variant_2: '#473bf0', // blue-ish
   text_variant_3: '#CDCDE0', // white-ish-greyish
   text_variant_4: 'rgba(255, 160, 1, 0.8)', // white-ish-greyish

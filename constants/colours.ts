ce44cfa1da6f6c6ce95343de5fd39aafa7681995
Okpainmo <okpainmoandrew@gmail.com>
2024-09-12 20:54:46 +0100

    chore: colours update(s)

--- constants/colours.ts
+++ constants/colours.ts
@@ -4,6 +4,7 @@ export const default_light_backgrounds = {
   background_variant_3: '#CDCDE0', // white-ish-greyish
   background_variant_4: 'rgba(255, 160, 1, 0.8)', // white-ish-greyish
   background_variant_5: '#dbeafe', // light-blue
+  background_variant_6: 'rgba(29, 29, 43, 0.35)', // transparent(showing dark_blue/slate background)
 };
 
 export const default_light_texts = {

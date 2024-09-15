f2107507d85d3a712131b23d4478feb59ccc3159
Okpainmo <okpainmoandrew@gmail.com>
2024-09-15 22:07:48 +0100

    chore: constants(colour) updatE

--- constants/colours.ts
+++ constants/colours.ts
@@ -4,7 +4,7 @@ export const default_light_backgrounds = {
   background_variant_3: '#CDCDE0', // white-ish-greyish
   background_variant_4: 'rgba(255, 160, 1, 0.8)', // white-ish-greyish
   background_variant_5: '#dbeafe', // light-blue
-  background_variant_6: 'rgba(29, 29, 43, 0.35)', // transparent(showing dark_blue/slate background)
+  background_variant_6: 'rgba(29, 29, 43, 0.25)', // transparent(showing dark_blue/slate background)
 };
 
 export const default_light_texts = {
@@ -13,4 +13,5 @@ export const default_light_texts = {
   text_variant_3: '#CDCDE0', // white-ish-greyish
   text_variant_4: 'rgba(255, 160, 1, 0.8)', // white-ish-greyish
   text_variant_5: '#ffffff', // black
+  text_variant_6: '#dbeafe', // light-blue
 };
